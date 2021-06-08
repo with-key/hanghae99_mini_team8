640943a27c04201815559d19d160bb97948cb23e
dusgn <33680794+gilthub@users.noreply.github.com>
2021-06-08 11:59:32 +0900

    #2 로그인, 회원가입 랜딩 페이지 작성 및 메서드 중복 해결

--- app.py
+++ app.py
@@ -5,23 +5,36 @@ app = Flask(__name__)
 
 @app.route('/')
 def main_page():
-	return '메인페이지 랜딩'
+    return '메인페이지 랜딩'
+
+
+@app.route('/login_page')
+def login_page():
+    return '로그인 랜딩'
+
+
+@app.route('/signup_page')
+def signup_page():
+    return '회원가입 랜딩'
+
 
 @app.route('/signup', methods=['POST'])
-def sign_up():
-	#회원가입
-	return '회원정보 DB에 저장후 jsonify에 success 반환'
+def signup():
+    # 회원가입
+    return '회원정보 DB에 저장후 jsonify에 success 반환'
+
 
 @app.route('/login', methods=['POST'])
-def log_in():
-	#로그인
-	return '로그인 성공 or 로그인 실패 반환'
+def login():
+    # 로그인
+    return '로그인 성공 or 로그인 실패 반환'
+
 
 @app.route('/signup/check_dup', methods=['POST'])
-def log_in():
-	#회원가입시 아이디 중복 체크
-	return '아이디가 중복시 success 반환'
+def check_dup():
+    # 회원가입시 아이디 중복 체크
+    return '아이디가 중복시 success 반환'
 
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
