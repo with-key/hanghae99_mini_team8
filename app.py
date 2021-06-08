4136266646337f8d7a4c91f63b6f18ab6f340c90
with-key <rem9238@kakao.com>
2021-06-08 14:00:08 +0900

    debug mode on, render template edit

--- app.py
+++ app.py
@@ -1,40 +1,50 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
+#############################
+###       router          ###
+#############################
+
+
+@app.route("/")
 def main_page():
-    return render_template('메인페이지 렌딩')
+    return render_template("home.html")
 
 
-@app.route('/login_page')
+@app.route("/login_page")
 def login_page():
-    return render_template('로그인페이지 랜딩')
+    return render_template("login.html")
 
 
-@app.route('/signup_page')
+@app.route("/signup_page")
 def signup_page():
-    return render_template('회원가입 페이지 렌딩')
+    return render_template("sign_up.html")
 
 
-@app.route('/signup', methods=['POST'])
+#############################
+###        API            ###
+#############################
+
+
+@app.route("/signup", methods=["POST"])
 def signup():
     # 회원가입
-    return '회원정보 DB에 저장후 jsonify에 success 반환'
+    return "회원정보 DB에 저장후 jsonify에 success 반환"
 
 
-@app.route('/login', methods=['POST'])
+@app.route("/login", methods=["POST"])
 def login():
     # 로그인
-    return '로그인 성공 or 로그인 실패 반환'
+    return "로그인 성공 or 로그인 실패 반환"
 
 
-@app.route('/signup/check_dup', methods=['POST'])
+@app.route("/signup/check_dup", methods=["POST"])
 def check_dup():
     # 회원가입시 아이디 중복 체크
-    return '아이디가 중복시 success 반환'
+    return "아이디가 중복시 success 반환"
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)  # debug mode on, 사용하면 server restart 불필요
