fe288b6a917002b733f97292f4b397d744c61f9f
wanhochio <dhksgh9506@icloud.com>
2021-06-08 22:00:16 +0900

    #9 redirect 되는 url을 login -> login_page로 수정

--- app.py
+++ app.py
@@ -24,9 +24,9 @@ def main_page():
         user_info = db.users.find_one({"username": payload["id"]})
         return render_template("home.html", user_info=user_info)
     except jwt.ExpiredSignatureError:
-        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+        return redirect(url_for("login_page", msg="로그인 시간이 만료되었습니다."))
     except jwt.exceptions.DecodeError:
-        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+        return redirect(url_for("login_page", msg="로그인 정보가 존재하지 않습니다."))
 
 
 @app.route("/login_page")
