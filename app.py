9f150a76d15ea4c020d6682c671e5d2f3604ca50
dusgn <33680794+gilthub@users.noreply.github.com>
2021-06-08 10:01:15 +0900

    #2로그인실행작성

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ app = Flask(__name__)
 def main_page():
 	return '메인페이지 랜딩'
 
+@app.route('/login', methods=['POST'])
+def login():
+	#로그인
+	return '로그인 성공 or 로그인 실패 반환'
+
+
 
 if __name__ == '__main__':
 	app.run()
