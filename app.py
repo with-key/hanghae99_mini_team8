983a4df7d8a6e49565fa3346a44516457a5fcdb3
dusgn <33680794+gilthub@users.noreply.github.com>
2021-06-08 10:03:55 +0900

    #2회원가입 실행 작성

--- app.py
+++ app.py
@@ -7,8 +7,13 @@ app = Flask(__name__)
 def main_page():
 	return '메인페이지 랜딩'
 
+@app.route('/signup', methods=['POST'])
+def sign_up():
+	#회원가입
+	return '회원정보 DB에 저장후 jsonify에 success 반환'
+
 @app.route('/login', methods=['POST'])
-def login():
+def log_in():
 	#로그인
 	return '로그인 성공 or 로그인 실패 반환'
 
