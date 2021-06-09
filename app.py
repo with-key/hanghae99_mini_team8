01296c2401391a4c5c280641671e0b31bbd4bfb4
dusgn <33680794+gilthub@users.noreply.github.com>
2021-06-09 15:41:32 +0900

    #4 상세페이지 작성

--- app.py
+++ app.py
@@ -166,5 +166,28 @@ def posting():
     except jwt.exceptions.DecodeError:
         return redirect(url_for("login"))
 
+
+@app.route('/post/<postid>')
+def post_detail(postid):
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        post_id = db.posts.find_one({'post_id': postid})
+        user_id = payload['id']
+        status = (user_id == post_id['userid'])
+
+        # 로그인아이디와 해당 포스트 작성자가 일치하면 True > true 받으면 수정 삭제 버튼 노출
+
+        post_info = db.posts.find_one({'post_id': postid})
+
+        # 해당 포스트 아이디에 대한 posts 데이터베이스 결과값 리스트로 반환
+        # return jsonify({'result': 'success', 'status': status, 'post_info': post_info})
+        return render_template('detail.html', status=status, post_info=post_info)
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login_page", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login_page", msg="로그인 정보가 존재하지 않습니다."))
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
